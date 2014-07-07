b0c6967cc9a62905337b027657421de5a54f00cb
slayoo <slayoo@igf.fuw.edu.pl>
2014-07-07 17:40:16 +0200

    updates

--- make.sh
+++ make.sh
@@ -6,9 +6,9 @@ rm -f *.html
 echo "-- generating..."
 ronn --html --style=toc --manual="LIBMPDATA++" --organization="University of Warsaw" libmpdata++.ronn
 ronn --html --style=toc --manual="LIBCLOUDPH++" --organization="University of Warsaw" libcloudph++.ronn
-ronn --html --manual="" --organization="University of Warsaw" foss.ronn
+ronn --html --manual="" --organization="University of Warsaw" team.ronn
 
 echo "-- uploading..."
 scp libmpdata++.html slayoo@burza.igf.fuw.edu.pl:~/LIBMPDATAXX/index.html
 scp libcloudph++.html slayoo@burza.igf.fuw.edu.pl:~/LIBCLOUDPHXX/index.html
-scp foss.html slayoo@burza.igf.fuw.edu.pl:~/FOSS/index.html
+scp team.html slayoo@burza.igf.fuw.edu.pl:~/FOSS/index.html
