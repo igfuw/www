75317c8f2c9f09da45d775009a446225c6ac5047
slayoo <slayoo@igf.fuw.edu.pl>
2014-07-08 12:50:34 +0200

    adding paper list

--- make.sh
+++ make.sh
@@ -6,7 +6,7 @@ rm -f *.html
 echo "-- generating..."
 ronn --html --style=toc --manual="LIBMPDATA++" --organization="University of Warsaw" libmpdata++.ronn
 ronn --html --style=toc --manual="LIBCLOUDPH++" --organization="University of Warsaw" libcloudph++.ronn
-ronn --html --manual="" --organization="University of Warsaw" team.ronn
+ronn --html --style=toc --manual="" --organization="University of Warsaw" team.ronn
 
 echo "-- uploading..."
 scp libmpdata++.html slayoo@burza.igf.fuw.edu.pl:~/LIBMPDATAXX/index.html
