c29d3465244d8dbc91b43ea76221e893341cf0c6
slayoo <slayoo@igf.fuw.edu.pl>
2014-12-12 11:01:41 +0100

    sync

--- make.sh
+++ make.sh
@@ -7,6 +7,7 @@ echo "-- generating..."
 ronn --html --style=toc --manual="LIBMPDATA++" --organization="University of Warsaw" libmpdata++.ronn
 ronn --html --style=toc --manual="LIBCLOUDPH++" --organization="University of Warsaw" libcloudph++.ronn
 ronn --html --style=toc --manual="" --organization="University of Warsaw" team.ronn
+#ronn --html --style=toc --manual="" --organization="University of Warsaw" workshop2015.ronn
 
 echo "-- uploading..."
 scp libmpdata++.html slayoo@burza.igf.fuw.edu.pl:~/LIBMPDATAXX/index.html
